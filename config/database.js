663abd3c6f8a99f5e72c4b359f4a9e84ae88dde0
GonzaEnrique27 <90418763+GonzaEnrique27@users.noreply.github.com>
2024-10-15 22:21:17 -0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,8 +1,11 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('ecommerce', 'root', '', {
-  host: 'localhost',
+
+const sequelize = new Sequelize('ecommerce', 'admin', 'b3v1566700844', {
+  host: 'database-2.cluo642yi1q5.us-east-2.rds.amazonaws.com', 
   dialect: 'mysql',
+  port: 3306,
+  logging: false, 
 });
 
 module.exports = sequelize;
